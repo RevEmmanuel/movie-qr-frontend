8f237e0340c0dcbccc625b09b6b28c78472809c6
RevEmmanuel <adeolaae1@gmail.com>
2023-10-11 09:43:53 +0100

    images

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -24,7 +24,7 @@ export default function MovieCard(props) {
               }}
             />
 
-            {/* loader for image */}
+            {/* loader for images */}
             <div
               className={`${
                 hasLoaded ? "hidden" : "block"
