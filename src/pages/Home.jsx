3e7d91a49ddba0f7a1f2f1f8df8961450a03f56e
RevEmmanuel <adeolaae1@gmail.com>
2023-10-11 11:11:12 +0100

    finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useQRCode } from 'react-qrcode';
 
 export default function Home() {
-    // const [result, setResults] = useState('https://qr-code-generator-nestjs-production.up.railway.app/movies/2938af');
-    // const [reloadTimer, setReloadTimer] = useState(10); // Initial timer value in seconds
-    // const [dataUrl, setDataUrl] = useState('');
+    const [result, setResults] = useState('');
+    const [reloadTimer, setReloadTimer] = useState(10); // Initial timer value in seconds
 
-    // Use the useQRCode hook with the result state to generate the dataUrl
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                console.log('called');
+                const response = await axios.get('https://qr-code-generator-nestjs-production.up.railway.app/qr-code');
+                setResults(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    const data = useQRCode('https://movie-qr-frontend.vercel.app/2938af');
-    // useEffect(() => {
-    //     setDataUrl(data);
-    //     console.log('called ooo');
-    // }, [result]);
+        // Call the API initially
+        fetchData();
 
-    {/*useEffect(() => {*/}
-    //     const fetchData = async () => {
-    //         try {
-    //             console.log('called');
-    //             const response = await axios.get('https://qr-code-generator-nestjs-production.up.railway.app/qr-code');
-    {/*            setResults(`https://qr-code-generator-nestjs-production.up.railway.app/movies/${response.data}`);*/}
-    {/*        } catch (error) {*/}
-    {/*            console.log(error);*/}
-    {/*        }*/}
-    {/*    };*/}
+        // Set up an interval to call the API every 10 seconds
+        const intervalId = setInterval(() => {
+            fetchData();
+            setReloadTimer(10); // Reset the timer to 10 seconds after each fetch
+        }, 10000);
 
-    {/*    // Call the API initially*/}
-    //     fetchData();
-    //
-    {/*    // Set up an interval to call the API every 10 seconds*/}
-    {/*    const intervalId = setInterval(() => {*/}
-    {/*        fetchData();*/}
-    {/*        setReloadTimer(10); // Reset the timer to 10 seconds after each fetch*/}
-    {/*    }, 10000);*/}
+        // Update the timer every second
+        const timerId = setInterval(() => {
+            setReloadTimer(prevTimer => prevTimer - 1);
+        }, 1000);
 
-    //     // Update the timer every second
-    //     const timerId = setInterval(() => {
-    //         setReloadTimer(prevTimer => prevTimer - 1);
-    //     }, 1000);
-    //
-    //     // Clean up the intervals when the component unmounts
-    //     return () => {
-    //         clearInterval(intervalId);
-    //         clearInterval(timerId);
-    //     };
-    // }, []);
+        // Clean up the intervals when the component unmounts
+        return () => {
+            clearInterval(intervalId);
+            clearInterval(timerId);
+        };
+    }, []);
 
     return (
         <div>
-            <img src={data} alt="qr-code" />
-            <p>Reload in 10 seconds</p>
+            <img src={result} alt="qr-code" />
+            <p>Reload in {reloadTimer} seconds</p>
         </div>
     );
 }
