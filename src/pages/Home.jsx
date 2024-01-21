1483752da70734c14c6e7ff12e84aaeb5f1890be
RevEmmanuel <adeolaae1@gmail.com>
2024-01-21 15:03:49 +0100

    removed timer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 export default function Home() {
     const [result, setResults] = useState('');
-    const [reloadTimer, setReloadTimer] = useState(10); // Initial timer value in seconds
 
     useEffect(() => {
         const fetchData = async () => {
@@ -14,32 +13,12 @@ export default function Home() {
                 console.log(error);
             }
         };
-
-        // Call the API initially
         fetchData();
-
-        // Set up an interval to call the API every 10 seconds
-        const intervalId = setInterval(() => {
-            fetchData();
-            setReloadTimer(10); // Reset the timer to 10 seconds after each fetch
-        }, 10000);
-
-        // Update the timer every second
-        const timerId = setInterval(() => {
-            setReloadTimer(prevTimer => prevTimer - 1);
-        }, 1000);
-
-        // Clean up the intervals when the component unmounts
-        return () => {
-            clearInterval(intervalId);
-            clearInterval(timerId);
-        };
     }, []);
 
     return (
         <div>
             <img src={result} alt="qr-code" />
-            <p>Reload in {reloadTimer} {reloadTimer === 1 ? 'second' : 'seconds'}</p>
         </div>
     );
 }
